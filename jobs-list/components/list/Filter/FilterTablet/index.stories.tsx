8f807d9449a7ef37e85efe629616e465ca95b19e
rlittle-code-test <rdlittle25+1@gmail.com>
2021-04-26 19:31:18 -0400

    feat: add `FilterTablet` component

---
+++ jobs-list/components/list/Filter/FilterTablet/index.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import FilterTablet from '.';
+
+export default {
+  title: 'Components/List/Filter/FilterTablet',
+  component: FilterTablet,
+  parameters: {
+    docs: {
+      description: {
+        component: 'A FilterTablet component',
+      },
+    },
+  },
+};
+export const primary = () => {
+  return <FilterTablet filterText={'Frontend'} />;
+};
