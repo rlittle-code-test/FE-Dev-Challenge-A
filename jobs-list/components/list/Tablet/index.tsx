01ecac9f3653e1b985d8342b1831f97dc364e028
rlittle-code-test <rdlittle25+1@gmail.com>
2021-04-26 19:29:56 -0400

    feat: add `Tablet` component

--- jobs-list/components/list/Tablet/index.tsx
+++ jobs-list/components/list/Tablet/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { stitches } from '../../../config/stitches.config';
 
-const StyledTablet = stitches.styled('span', {
+const StyledTablet = stitches.styled('button', {
   background: '$bluegray200',
+  border: 'none',
   borderRadius: '0.2rem',
   color: '$blue400',
   fontSize: '15px',
   fontWeight: 700,
+  outline: 'none',
   padding: '0.4rem',
   '&:hover': {
     background: '$blue400',
@@ -14,8 +16,12 @@ const StyledTablet = stitches.styled('span', {
   },
 });
 
-export function Tablet(props: React.PropsWithChildren<{}>) {
-  return <StyledTablet>{props.children}</StyledTablet>;
+export type TabletProps = {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+export function Tablet(props: React.PropsWithChildren<TabletProps>) {
+  return <StyledTablet onClick={props.onClick}>{props.children}</StyledTablet>;
 }
 
 export default Tablet;
